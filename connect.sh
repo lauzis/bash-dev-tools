e1dca7d9639c311b29a5905f8188428d15e4f5ab
Aivars Lauzis <aivars.lauzis@awave.com>
2018-08-15 11:36:36 +0300

    connect command added (connects to the mysql)

---
+++ connect.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+CURRENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SETTINGSFILE=$CURRENTDIR"/config.cfg"
+
+#checks if settings file exists if not exits
+if [ -f "$SETTINGSFILE" ]
+then
+    #reading settings
+	source $SETTINGSFILE
+else
+	echo ERR:1 Settings file $SETTINGSFILE not found... sorry, can not continue...
+	exit
+fi	
+
+
+
+mysql -u $mysql_user -p"${mysql_password}"
